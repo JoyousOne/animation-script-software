865462245ed708a8c9124d292a22cdb1c24d21dc
Lajoie, Samuel <lajoie.samuel@courrier.uqam.ca>
2025-07-20 17:12:13 -0400

    update types.rs

--- src/common/types.rs
+++ src/common/types.rs
@@ -3,13 +3,22 @@ pub struct Pixel {
     g: u8,
     b: u8,
 }
+
 impl Pixel {
     pub fn new(r: u8, g: u8, b: u8) -> Self {
         Pixel { r, g, b }
     }
 }
 
+type Color = Pixel;
+
 pub struct Position {
     x: usize,
     y: usize,
+}
+
+impl Position {
+    pub fn new(x: usize, y: usize) -> Self {
+        Position { x, y }
+    }
 }
