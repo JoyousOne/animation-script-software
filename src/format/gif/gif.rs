95593fdfe2d1b65dac72035486939bf6d8e8ed4d
Lajoie, Samuel <lajoie.samuel@courrier.uqam.ca>
2025-07-21 18:43:44 -0400

    added image usage in gif

--- src/format/gif/gif.rs
+++ src/format/gif/gif.rs
@@ -1,42 +1,49 @@
-use crate::common::types::{Color, Image};
-use std::{cmp::min, fs::File, io::Write};
+use crate::common::types::Color;
+use crate::format::gif::gif_image::Image;
+use std::{cell::RefCell, cmp::min, fs::File, io::Write, rc::Rc};
 
 pub const SIGNATURE: [u8; 3] = *b"GIF";
 pub const VERSION: [u8; 3] = *b"89a";
 pub const TRAILER_MARKER: u8 = 0x3B;
 
-const EXTENSIONS_INTRODUCER: u8 = 0x21;
-const GRAPHIC_CONTROL_LABEL: u8 = 0xF9;
-
-struct Gif {
+pub struct Gif {
     height: u16,
     width: u16,
-    global_color_table: Vec<Color>,
+    global_color_table: Rc<RefCell<Vec<Color>>>,
     images: Vec<Image>, // TODO add image type
 }
 
 impl Gif {
-    fn new(height: u16, width: u16, use_global_color_table: bool) -> Self {
+    pub fn new(height: u16, width: u16, use_global_color_table: bool) -> Self {
         Gif {
             height,
             width,
-            global_color_table: vec![],
-            images: vec![],
+            global_color_table: Rc::new(RefCell::new(Vec::new())),
+            images: Vec::new(),
         }
     }
 
     fn get_global_color_table_size_bits(&self) -> u8 {
-        let color_count = self.global_color_table.len();
+        let color_count = self.global_color_table.borrow().len();
         let bit_size = ((color_count as f64).log2().ceil() - 1.0) as u8;
 
         min(bit_size, 0x07)
     }
 
-    fn add_image() {
-        // TODO
+    // TODO for now only global table
+    pub fn add_image(&mut self) -> &mut Image {
+        let new_image = Image::new(
+            self.height,
+            self.width,
+            Some(Rc::clone(&self.global_color_table)),
+        );
+
+        self.images.push(new_image);
+
+        self.images.last_mut().unwrap()
     }
 
-    fn write_to_file(&mut self, filename: String) -> Result<(), Box<dyn std::error::Error>> {
+    pub fn write_to_file(&mut self, filename: &str) -> Result<(), Box<dyn std::error::Error>> {
         let mut file_contents = vec![];
         let global_color_bits = self.get_global_color_table_size_bits();
 
@@ -59,28 +66,34 @@ impl Gif {
         file_contents.push(0);
 
         // add padding to the color table
-        let padding_size = (1 << (global_color_bits + 1)) - self.global_color_table.len();
+        {
+            let mut global_color_table = self.global_color_table.borrow_mut();
 
-        for _ in 0..padding_size {
-            self.global_color_table.push(Color { r: 0, g: 0, b: 0 });
-        }
+            // Filling global color table with padding
+            let padding_size = (1 << (global_color_bits + 1)) - global_color_table.len();
+            for _ in 0..padding_size {
+                global_color_table.push(Color::new(0, 0, 0));
+            }
 
-        // add colors
-        for color in self.global_color_table {
-            file_contents.extend(color.as_bytes());
+            // add colors
+            for color in global_color_table.iter() {
+                file_contents.extend(color.as_bytes());
+            }
         }
 
         // add images
-        for image in self.images {
-            file_contents.push(image.get_image_descriptor()); // TODO check with image code
-            file_contents.push(image.get_image_data()); // TODO check with image code
+        for image in &self.images {
+            file_contents.extend_from_slice(&image.as_bytes());
         }
 
         // add gif end
         file_contents.push(TRAILER_MARKER);
 
+        // DEBUG
+        // println!("file_contents: {:02X?}", file_contents);
+
         // write to file
-        let file = File::create(filename)?;
+        let mut file = File::create(filename)?;
         file.write_all(&file_contents)?;
 
         Ok(())
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
+use crate::{
+    common::types::{Pixel, Position},
+    format::gif::gif::Gif,
+};
+
 mod common;
 mod format;
 
 fn main() {
-    println!("Hello, world!");
+    let (height, width) = (10u16, 10u16);
+    let mut gif = Gif::new(height, width, true);
+
+    let new_img = gif.add_image();
+
+    let white = Pixel::new(255, 255, 255);
+    let red = Pixel::new(255, 0, 0);
+    let blue = Pixel::new(0, 0, 255);
+    // let black = Pixel::new(0, 0, 0);
+
+    new_img
+        .fill(white.clone())
+        .fill(red)
+        .draw_rectangle(Position::new(5, 0), Position::new(10, 5), blue.clone())
+        .draw_rectangle(Position::new(0, 5), Position::new(5, 10), blue.clone())
+        .draw_rectangle(Position::new(3, 3), Position::new(7, 7), white.clone());
+
+    let filename = "./test.gif";
+    let _ = gif.write_to_file(filename);
+    // println!("Hello, world!");
 }
