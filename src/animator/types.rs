3a992a782fb2cb39a8eef819ce3be3199237c0f2
MaximeGazze <maxime.gazze@videotron.ca>
2025-08-02 13:30:39 -0400

    added rotation operators

--- src/animator/types.rs
+++ src/animator/types.rs
@@ -1,4 +1,7 @@
-use std::ops::{Add, Mul, Sub};
+use std::{
+    f64::consts::PI,
+    ops::{Add, Mul, Sub},
+};
 
 pub type CoordinateValue = f64;
 
@@ -40,10 +43,10 @@ impl Sub for Point {
     }
 }
 
-impl Mul<f64> for Point {
+impl Mul<CoordinateValue> for Point {
     type Output = Self;
 
-    fn mul(self, scalar: f64) -> Self::Output {
+    fn mul(self, scalar: CoordinateValue) -> Self::Output {
         Self {
             x: self.x * scalar,
             y: self.y * scalar,
@@ -63,15 +66,85 @@ pub enum Color {
 #[derive(Debug, Clone, Copy)]
 pub enum Length {
     Pixel(u32),
-    Percentage(u32),
+    Percentage(f64),
     ScreenHeight(),
 }
 
 #[derive(Debug, Clone, Copy)]
 pub enum Rotation {
-    Degree(f64),
     Turn(f64),
+    Degree(f64),
     Radian(f64),
+}
+
+impl Rotation {
+    fn value(&self) -> f64 {
+        match self {
+            Self::Turn(value) => *value,
+            Self::Degree(value) => *value,
+            Self::Radian(value) => *value,
+        }
+    }
+
+    pub fn as_degree(self) -> Self {
+        match self {
+            Self::Turn(value) => Self::Degree(value * 360.0),
+            Self::Degree(_) => self,
+            Self::Radian(value) => Self::Degree(value * (180.0 / PI)),
+        }
+    }
+
+    pub fn as_turn(self) -> Self {
+        match self {
+            Self::Turn(_) => self,
+            Self::Degree(value) => Self::Turn(value / 360.0),
+            Self::Radian(value) => Self::Turn(value / (2.0 * PI)),
+        }
+    }
+
+    pub fn as_radian(self) -> Self {
+        match self {
+            Self::Turn(value) => Self::Radian(value * (2.0 * PI)),
+            Self::Degree(value) => Self::Radian(value * (PI / 180.0)),
+            Self::Radian(_) => self,
+        }
+    }
+}
+
+impl Add for Rotation {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        match self {
+            Self::Turn(value) => Self::Turn(value + other.as_turn().value()),
+            Self::Degree(value) => Self::Degree(value + other.as_degree().value()),
+            Self::Radian(value) => Self::Radian(value + other.as_radian().value()),
+        }
+    }
+}
+
+impl Sub for Rotation {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        match self {
+            Self::Turn(value) => Self::Turn(value - other.as_turn().value()),
+            Self::Degree(value) => Self::Degree(value - other.as_degree().value()),
+            Self::Radian(value) => Self::Radian(value - other.as_radian().value()),
+        }
+    }
+}
+
+impl Mul<f64> for Rotation {
+    type Output = Self;
+
+    fn mul(self, scalar: f64) -> Self::Output {
+        match self {
+            Self::Turn(value) => Self::Turn(value * scalar),
+            Self::Degree(value) => Self::Degree(value * scalar),
+            Self::Radian(value) => Self::Radian(value * scalar),
+        }
+    }
 }
 
 #[derive(Debug, Clone, Copy)]
